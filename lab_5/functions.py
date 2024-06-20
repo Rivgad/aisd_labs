8fcbcf46c1e2a229690c6ea0f2d85cfae36059f4
rivgad <losvermo@gmail.com>
2024-06-20 15:09:17 +0300

    fix lab 5 sort algs

--- lab_5/functions.py
+++ lab_5/functions.py
@@ -5,37 +5,36 @@ def generate_random_array(size):
     return [random.randint(-10000, 10000) for _ in range(size)]
 
 
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(array):
+    n = len(array) - 1
 
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    for i in range(n,0,-1):
+        for j in range(i):
+            if array[j]>array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
 
-    return arr
+    return array
 
 
-def selection_sort(arr):
-    n = len(arr)
+def selection_sort(array):
+    n = len(array) - 1
 
     for i in range(n):
         min_index = i
         for j in range(i + 1, n):
-            if arr[min_index] > arr[j]:
+            if array[j] < array[min_index]:
                 min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+        array[i], array[min_index] = array[min_index], array[i]
 
-    return arr
+    return array
 
 
-def insertion_sort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i-1
+        while array[j] > key and j >= 0:
+            array[j+1] = array[j]
             j -= 1
-        arr[j + 1] = key
-    return arr
+        array[j+1] = key
+    return array
